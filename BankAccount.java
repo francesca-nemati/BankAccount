ba3c0edd708ce4852ba1907cc3dadd6ff21cf4a4
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2020-10-26 17:07:04 -0400

    constructor added

--- BankAccount.java
+++ BankAccount.java
@@ -3,4 +3,14 @@ public class BankAccount {
   private double balance;
   private int accountID;
   private String password;
+
+  //Constructor: named same as class
+  public BankAccount(int ID, String pass) {
+    accountID = ID;
+    password = pass;
+    balance = 0.0;
+  }
+
+  //Methods 
+
 }
